a362ed01fed2a5c5d43e2601f86fe931c11d8352
Michail <mihasik323@gmail.com>
2021-07-02 14:41:10 +0300

    Replaced css modules with BEM classes

--- src/components/ControlPanel/ControlPanel.tsx
+++ src/components/ControlPanel/ControlPanel.tsx
@@ -1,32 +1,30 @@
-import Header from "../Header/Header";
-import { AiOutlineUnorderedList, AiOutlinePlus } from "react-icons/ai";
-import classes from "./ControlPanel.module.css";
-import Groups from "./Groups/Groups";
+import Header from '../Header/Header';
+import { AiOutlineUnorderedList, AiOutlinePlus } from 'react-icons/ai';
+import './ControlPanel.css';
+import Groups from './Groups/Groups';
 
 const ControlPanel: React.FC = () => {
   return (
-    <div className={classes.controlPanel}>
+    <div className='c-panel'>
       <Header />
 
-      <div className={classes.controls}>
-        <div className={classes.groups}>
-          <a className="icon">
+      <div className='c-panel__controls'>
+        <div className='c-panel__groups'>
+          <a className='icon'>
             <AiOutlineUnorderedList size={24} /> <span>Все задачи</span>
           </a>
 
           <Groups />
 
-          <a className="icon">
+          <a className='icon'>
             <AiOutlinePlus size={24} /> <span>Добавить группу</span>
           </a>
         </div>
 
-        <div className={classes.add}>
-          <button className={`${classes.addBtn} icon`} type='button'>
-            <AiOutlinePlus size={24} />
-            <span>Новая задача</span>
-          </button>
-        </div>
+        <button className='c-panel__addBtn icon' type='button'>
+          <AiOutlinePlus size={24} />
+          <span>Новая задача</span>
+        </button>
       </div>
     </div>
   );
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,17 @@
 import logo from "../../assets/logo/logo.svg";
 
-import classes from './Header.module.css';
+import './Header.css';
 
 const Header: React.FC = () => {
   return (
-    <header className={classes.header}>
-      <a className={`${classes.logo} icon`} href="/">
+    <header className='header'>
+      <a className='header__logo icon' href="/">
         <img src={logo} alt="" width="44" height="44" />
         <span>TODO</span>
       </a>
-      <div className={classes.greeting}>
+      <div className='header__greeting'>
         <div>Привет, <span>Имя!</span></div>
-        <button className={classes.logOut} type="button">Выйти</button>
+        <button className='header__logout' type="button">Выйти</button>
       </div>
     </header>
   );
